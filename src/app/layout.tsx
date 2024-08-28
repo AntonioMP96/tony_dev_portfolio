a4462fffbbcf01c037a9045008a01d293b676204
Antonio Martinez <jmlegoman2@gmail.com>
2024-08-27 22:28:53 -0600

    - adding footer component an main classnames in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Footer } from '@/components'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Antonio Martinez",
-  description: "Desarrollador Fullstack",
+  description: "Desarrollador Fullstack"
 };
 
 export default function RootLayout({
@@ -17,7 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <main className="h-screen max-h-screen py-6 sm:py-8 lg:py-12 px-4 md:px-8 flex flex-col items-center">
         {children}
+        </main>
+        <Footer/>
       </body>
     </html>
   );
