12d1427bd00695910094d5c3a015abd012d60f69
Tony <jmlegoman2@gmail.com>
2024-08-29 08:56:53 -0600

    - footer margin top incremented

--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link'
 
 export const Footer = () => {
     return (
-        <div className='flex w-full justify-center text-xs mb-10'>
+        <footer className='flex w-full justify-center text-xs my-10'>
             <Link href="/"
             className='hover:text-blue-500 transition-all'
             >
                 <span className='antialiased font-bold'>Antonio Martínez</span>
                 <span> | Fullstack Developer</span>
             </Link>
-        </div>
+        </footer>
     )
 }
