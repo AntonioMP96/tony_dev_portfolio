45f1d3157bfdd9cc0e301ba0c9ef233e0aeb16b9
Antonio Martinez <jmlegoman2@gmail.com>
2024-09-02 21:44:44 -0600

    - Adding data from data file - Adding a view all projects anchor

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { Header } from '@/components'
+import projectsData from '@/lib/projects'
+
 
 interface Props {
     params: {
@@ -8,33 +10,13 @@ interface Props {
     }
 }
 
-const projectData: Record<string, { 
-    title: string, short_description: string, 
-    long_description: string, link: string, image: string 
-}> = {
-    'dentista_coapa' : {
-        title: 'Dentista Coapa',
-        short_description: 'A promotional site for a mexican local dentist located in Villa Coapa, Mexico City.',
-        long_description: "A promotional site for a mexican local dentist located in Villa Coapa, Mexico City. Where people can know more about the dentist's services, and schedule an appointment for any of the offered dental services.",
-        link: 'https://www.dentistacoapa.com',
-        image: 'https://res.cloudinary.com/dgp7gxmcu/image/upload/f_auto,q_auto/v1/tony_portfolio/nsssf5tl92lh4fk3tila'
-    },
-    'panacea': {
-        title: 'Panacea',
-        short_description: 'A slideshow page for a CBD products startup located in Mexico City.',
-        long_description: 'A slideshow page for a CBD products startup located in Mexico City. Where people can get to know more about CBD and its properties, and browse all the companie"s products.',
-        link: 'https://panacea-mexico-683mhyhv0-tony-dev96.vercel.app/',
-        image: 'https://res.cloudinary.com/dgp7gxmcu/image/upload/f_auto,q_auto/v1/tony_portfolio/etjjo3l1qrkufbd4uje7'
-    }
-}
-
 export default async function ProjectPage({params}: Props) {
     const { slug } = params
-    if (!projectData[slug]) {
+    if (!projectsData[slug]) {
         return <div>Project not found</div>
     }
 
-    const { title, short_description, long_description, link, image } = projectData[slug]
+    const { title, short_description, long_description, link, image } = projectsData[slug]
 
 
     return (
@@ -79,10 +61,22 @@ export default async function ProjectPage({params}: Props) {
                             <li>JavaScript</li>
                         </ul>
                     </span>
-                    <Link href={link} className={`inline-block rounded-lg border bg-gray-800 px-4 py-2 text-center text-sm font-semibold text-gray-100 outline-none ring-indigo-300 transition duration-200 hover:bg-gray-300 hover:text-gray-800 focus-visible:ring active:bg-gray-400 md:px-8 md:py-3 md:text-base`}>
-                        View&nbsp;live
-                    </Link>
+                    <a href={link} target="_blank" className={`inline-block rounded-lg border bg-gray-800 px-4 py-2 text-center text-sm font-semibold text-gray-100 outline-none ring-indigo-300 transition duration-200 hover:bg-gray-300 hover:text-gray-800 focus-visible:ring active:bg-gray-400 md:px-8 md:py-3 md:text-base`}>
+                        View&nbsp;live&nbsp;project
+                    </a>
                 </div>
+            </div>
+
+            <div className='mt-60'>
+                <Link href="/projects"
+                className={`inline-block 
+                rounded-lg border bg-transparent px-4 py-2 
+                text-center text-sm font-semibold text-gray-800 
+                outline-none ring-gray-800 transition duration-200 
+                hover:bg-gray-800 hover:text-gray-100 focus-visible:ring active:bg-gray-600 
+                md:px-8 md:py-3 md:text-base border-gray-800`}>
+                    View all projects
+                </Link>
             </div>
 
         </>
