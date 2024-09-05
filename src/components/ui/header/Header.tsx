b367d42cdfa004a456137e64daa20398c93e87b3
Antonio Martinez <jmlegoman2@gmail.com>
2024-09-04 22:55:56 -0600

    - Adding modal call to header

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -2,6 +2,9 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useState } from 'react'
+import { Modal } from '@/components'
+import { FaWhatsapp, FaEnvelope, FaPhoneAlt } from 'react-icons/fa'
 
 interface Props {
     title: string
@@ -12,6 +15,10 @@ interface Props {
 export const Header = ({title, subtitle}: Props) => {
 
     const pathname = usePathname()
+
+    const [isModalOpen, setModalOpen] = useState(false)
+    const openModal = () => setModalOpen(true)
+    const closeModal = () => setModalOpen(false)
 
     return (
         <header className='w-full max-w-screen-2xl'>
@@ -29,7 +36,9 @@ export const Header = ({title, subtitle}: Props) => {
                         Return&nbsp;home
                     </Link>
                     ) : 
-                    <button className="inline-block rounded-lg border bg-gray-800 px-4 py-2 text-center text-sm font-semibold text-gray-100 outline-none ring-indigo-300 transition duration-200 hover:bg-gray-300 hover:text-gray-800 focus-visible:ring active:bg-gray-400 md:px-8 md:py-3 md:text-base">
+                    <button 
+                    onClick={openModal}
+                    className="inline-block rounded-lg border bg-gray-800 px-4 py-2 text-center text-sm font-semibold text-gray-100 outline-none ring-indigo-300 transition duration-200 hover:bg-gray-300 hover:text-gray-800 focus-visible:ring active:bg-gray-400 md:px-8 md:py-3 md:text-base">
                         Contact
                     </button>
                 }
@@ -37,6 +46,21 @@ export const Header = ({title, subtitle}: Props) => {
             <h2 className="block md:hidden w-full text-gray-500 mb-4">
                 { subtitle }
             </h2>
+            <Modal isOpen={isModalOpen} onClose={closeModal}>
+                <h2 className='text-xl font-bold mb-5'>Contact Antonio</h2>
+                <p className={`mb-5`}>Select any of the following contact ways:</p>
+                <div className='flex justify-between gap-5'>
+                    <button className='border-2 border-gray-700 rounded-xl p-4 transition-all hover:text-white hover:bg-gray-800'>
+                        <FaWhatsapp size={60} />
+                    </button>
+                    <button className='border-2 border-gray-700 rounded-xl p-4 transition-all hover:text-white hover:bg-gray-800'>
+                        <FaEnvelope size={60} />
+                    </button>
+                    <button className='border-2 border-gray-700 rounded-xl p-4 transition-all hover:text-white hover:bg-gray-800'>
+                        <FaPhoneAlt size={60} />
+                    </button>
+                </div>
+            </Modal>
         </header>
     )
 }
