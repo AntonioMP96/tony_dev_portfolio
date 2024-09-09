0ffa9c5d5b753d6dfac7e894ac462618b4e86b12
Antonio Martinez <jmlegoman2@gmail.com>
2024-09-08 22:39:10 -0600

    - Adding the Tech element for the slider or the tech showcase

---
+++ src/components/ui/tech_button/TechButton.tsx
@@ -0,0 +1,35 @@
+'use client'
+import Image from 'next/image'
+
+interface Props {
+    image: string
+    title: string,
+    onClick?: () => void
+    hideName?: boolean
+}
+
+
+export const TechButton = ({image, title, onClick, hideName}: Props) => {
+
+
+    return (
+        <div className={`h-full aspect-square mx-5
+        ${hideName && 'cursor-pointer group'}
+        `} onClick={onClick}>
+            <Image
+            src={image}
+            alt={title}
+            width={100}
+            height={100}
+            className='h-full w-full aspect-square'
+            />
+            <p className={`text-center text-sm mt-2 transition-all
+            ${ hideName ? 'opacity-0 group-hover:opacity-100' : 'text-xl'}
+            `}>
+                {title}
+            </p>
+
+
+        </div>
+    )
+}
