6c4f43104f8ab704d614e62b07a5938e603b2136
Antonio Martinez <jmlegoman2@gmail.com>
2024-09-08 22:40:06 -0600

    - Adding the teck_stack data to the data library

---
+++ src/lib/tech_stack.ts
@@ -0,0 +1,78 @@
+const techStackData: Record<string, { 
+    title: string,  image: string, slug: string, type: string
+}> = {
+    'python' : {
+        title: 'Python',
+        image: 'https://res.cloudinary.com/dgp7gxmcu/image/upload/f_auto,q_auto/v1/tony_portfolio/zwqo1rzpljdht95pqaw0',
+        slug: 'python',
+        type: 'backend'
+    },
+    'javascript': {
+        title: 'JavaScript',
+        image: 'https://res.cloudinary.com/dgp7gxmcu/image/upload/f_auto,q_auto/v1/tony_portfolio/vlr9l5ruigkatkpo5skt',
+        slug: 'javascript',
+        type: 'both'
+    },
+    'django': {
+        title: 'Django',
+        image: 'https://res.cloudinary.com/dgp7gxmcu/image/upload/f_auto,q_auto/v1/tony_portfolio/emt6cvxz4iot1ikxxe35',
+        slug: 'django',
+        type: 'backend'
+    },
+    'nextjs': {
+        title: 'Nextjs',
+        image: 'https://res.cloudinary.com/dgp7gxmcu/image/upload/f_auto,q_auto/v1/tony_portfolio/o2add8fapxmmzwg0zni6',
+        slug: 'nextjs',
+        type: 'both'
+    },
+    'typescript': {
+        title: 'TypeScript',
+        image: 'https://res.cloudinary.com/dgp7gxmcu/image/upload/f_auto,q_auto/v1/tony_portfolio/ppbg0sp4s9e9i7uuqjve',
+        slug: 'typescript',
+        type: 'support'
+    },
+    'css': {
+        title: 'CSS',
+        image: 'https://res.cloudinary.com/dgp7gxmcu/image/upload/f_auto,q_auto/v1/tony_portfolio/svrinzws5x6nhif9mqnt',
+        slug: 'css',
+        type: ''
+    },
+    'html': {
+        title: 'HTML 5',
+        image: 'https://res.cloudinary.com/dgp7gxmcu/image/upload/f_auto,q_auto/v1/tony_portfolio/ckgiaqdflu25p5k44mbu',
+        slug: 'html_5',
+        type: ''
+    },
+    'react_native': {
+        title: 'React Native',
+        image: 'https://res.cloudinary.com/dgp7gxmcu/image/upload/f_auto,q_auto/v1/tony_portfolio/lqjdnnvemrfammfkeksu',
+        slug: 'react_native',
+        type: ''
+    },
+    'tailwind': {
+        title: 'Tailwind',
+        image: 'https://res.cloudinary.com/dgp7gxmcu/image/upload/f_auto,q_auto/v1/tony_portfolio/wtikuvvdbevkf8sprubn',
+        slug: 'tailwind',
+        type: ''
+    },
+    'docker': {
+        title: 'Docker',
+        image: 'https://res.cloudinary.com/dgp7gxmcu/image/upload/f_auto,q_auto/v1/tony_portfolio/vsi4ii9bskbs9v44ap8t',
+        slug: 'docker',
+        type: ''
+    },
+    'postgresql': {
+        title: 'PostgreSQL',
+        image: 'https://res.cloudinary.com/dgp7gxmcu/image/upload/f_auto,q_auto/v1/tony_portfolio/gkc7wgrbwdhhqbyhrdo6',
+        slug: 'postgresql',
+        type: ''
+    },
+    'mongodb': {
+        title: 'Mongo DB',
+        image: 'https://res.cloudinary.com/dgp7gxmcu/image/upload/f_auto,q_auto/v1/tony_portfolio/av3dnza88gsvrwla1z8p',
+        slug: 'mongo_db',
+        type: ''
+    }
+}
+
+export default techStackData
