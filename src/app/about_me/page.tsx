e586d0edc45899d7a5c25ea56ae41d6fd8c27c1d
Antonio Martinez <jmlegoman2@gmail.com>
2024-09-09 21:20:59 -0600

    - improving responsiveness of about me page

--- src/app/about_me/page.tsx
+++ src/app/about_me/page.tsx
@@ -17,22 +17,29 @@ export default function ProjectsPage() {
             <div className="flex flex-1 h-full w-full max-w-screen-2xl items-center justify-center relative">
                 <div className={`
                 absolute top-0 bottom-0 left-0 right-0 
-                grid md:grid-cols-6 md:grid-rows-4
+                grid grid-cols-1 md:grid-cols-6 
+                grid-rows-6 md:grid-rows-4
                 gap-4 md:gap-6 xl:gap-8
                 `}>
                     {/* Salute */}
-                    <div className={`p-4 bg-gray-800 text-white col-span-2 row-span-2 rounded-xl shadow-xl flex flex-col items-center justify-center`}>
-                        <h2 className='text-6xl xl:text-6xl 2xl:text-7xl font-bold'>
-                            Hi, I'm<br/>Tony 👋
-                            <p className="text-lg md:text-xl font-normal mt-3">Fullstack developer</p>
+                    <div className={`p-4 bg-gray-800 text-white 
+                        col-span-1 row-span-1
+                        md:col-span-2 md:row-span-2 
+                        rounded-xl shadow-xl flex flex-col items-center justify-center`}>
+                        <h2 className='flex md:flex-col text-4xl xl:text-5xl 2xl:text-7xl font-bold'>
+                            <span>Hi, I'm&nbsp;</span>
+                            <span>Tony 👋</span>
+                            {/* <p className="text-lg md:text-xl font-normal mt-3">Fullstack developer</p> */}
                         </h2>
                         
                     </div>
                     {/* About me */}
-                    <div className={`p-6 bg-gray-800 col-span-4 row-span-2 rounded-xl shadow-xl flex flex-col text-white
+                    <div className={`p-6 bg-gray-800 
+                    col-span-1 row-span-1
+                    md:col-span-4 md:row-span-2 rounded-xl shadow-xl flex flex-col text-white
                     overflow-hidden relative overflow-y-auto
                     `}>
-                        <div className="absolute h-[30%] right-0 left-0 bottom-0 bg-gradient-to-b from-transparent to-gray-800"></div>
+                        {/* <div className="absolute h-[30%] right-0 left-0 bottom-0 bg-gradient-to-b from-transparent to-gray-800"></div> */}
                         <h2 className='text-lg 2xl:text-2xl font-bold'>About me</h2>
                         <p className='2xl:text-xl mt-3'>
                             I'm a fullstack developer with a vast career in engineering and development.
@@ -45,44 +52,55 @@ export default function ProjectsPage() {
                         </p>
                     </div>
                     {/* Skills */}
-                    <div className={`bg-gray-800 col-span-2 row-span-1 rounded-xl shadow-xl p-6 text-white`}>
-                        <h2 className='text-lg md:text-2xl font-bold'>Skills</h2>
-                        <p className='mt-3 text-md xl:text-xl'>
-                            Web | Backend | API | Android | Web Apps
-                        </p>
+                    <div className={`bg-gray-800 
+                        col-span-1 row-span-1
+                        md:col-span-2 md:row-span-1 rounded-xl shadow-xl p-6 text-white flex justify-center items-center`}>
+                        <h2 className='text-xl md:text-2xl 2xl:text-4xl font-bold'>Fullstack Developer</h2>
                     </div>
                     {/* Contact */}
-                    <div className={`bg-gray-800 col-span-2 row-span-1 rounded-xl shadow-xl text-white text-6xl flex items-center justify-evenly gap-2 `}>
+                    <div className={`p-2 bg-gray-800 
+                    col-span-1 row-span-1
+                    md:col-span-2 md:row-span-1 rounded-xl shadow-xl text-white text-5xl xl:text-6xl flex items-center justify-evenly gap-2 `}>
                         <button 
                         // onClick={handleWhatsAppClick}
-                        className='border-2 border-gray-800 rounded-xl p-4 transition-all hover:bg-gray-900'>
+                        className='max-h-full aspect-square p-3 flex items-center justify-center border-2 border-gray-800 rounded-xl transition-all hover:bg-gray-900'>
                             <FaWhatsapp />
                         </button>
                         <button 
                         // onClick={handleEmailClick}
-                        className='border-2 border-gray-800 rounded-xl p-4 transition-all  hover:bg-gray-900'>
+                        className='max-h-full aspect-square p-3 flex items-center justify-center border-2 border-gray-800 rounded-xl transition-all hover:bg-gray-900'>
                             <FaEnvelope />
                         </button>
                         <button 
                         // onClick={handleDiscordClick}
-                        className='border-2 border-gray-800 rounded-xl p-4 transition-all hover:bg-gray-900'>
+                        className='max-h-full aspect-square p-3 flex items-center justify-center border-2 border-gray-800 rounded-xl transition-all hover:bg-gray-900'>
                             <FaDiscord />
                         </button>
                     </div>
                     {/* Proffesional experience */}
-                    <div className={`bg-gray-800 col-span-2 row-span-2 rounded-xl shadow-xl p-6 text-white relative overflow-hidden overflow-y-auto`}>
-                        <h2 className='text-lg md:text-2xl font-bold'>Professional experience</h2>
+                    <div className={`bg-gray-800 
+                    col-span-1 row-span-1
+                    md:col-span-2 md:row-span-2 rounded-xl shadow-xl p-6 text-white relative overflow-hidden overflow-y-auto`}>
+                        {/* <div className="absolute h-full right-0 left-0 bottom-0 bg-gradient-to-b from-transparent to-gray-800"></div> */}
+                        <h2 className='text-lg 2xl:text-2xl font-bold'>Professional experience</h2>
                         <ul className='mt-3 2xl:text-xl relative'>
                             <div className='h-full bg-white w-[1px] absolute left-[4px] top-[15px]'></div>
-                            <li className='ml-5 my-2 list-disc'>Fullstack developer | Cykadas LLC | Sept 2022 - Now </li>
-                            <li className='ml-5 my-2 list-disc'>Exploration geophysicist | Raptorodajes | May 2020 - June 2022</li>
-                            <li className='ml-5 my-2 list-disc'>Python Teacher | SAGFI UNAM | Jan 2020 - Mar 2020</li>
+                            <li className='ml-5 my-2 list-disc'>
+                                Fullstack developer @ 
+                                <a className="text-blue-500 hover:text-blue-600 transition-all" 
+                                href="https://www.cykadas.com">&nbsp;Cykadas LLC</a>
+                                &nbsp;| Sept 2022 - Now 
+                            </li>
+                            <li className='ml-5 my-2 list-disc'>Exploration geophysicist @ Raptorodajes | May 2020 - June 2022</li>
+                            <li className='ml-5 my-2 list-disc'>Python Teacher @ SAGFI UNAM | Jan 2020 - Mar 2020</li>
                         </ul>
                     </div>
                     {/* Tech Stack */}
-                    <div className={`bg-gray-800 col-span-4 row-span-1 rounded-xl shadow-xl p-6 text-white
+                    <div className={`bg-gray-800 
+                    col-span-1 row-span-1
+                    md:col-span-4 md:row-span-1 rounded-xl shadow-xl p-6 text-white
                         flex items-center`}>
-                        <h2 className='text-lg md:text-2xl font-bold'>Tech Stack</h2>
+                        <h2 className='text-lg 2xl:text-2xl font-bold'>Tech Stack</h2>
                         <TechSlackSlider />
                     </div>
                 </div>
